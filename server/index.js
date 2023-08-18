e0c10c065830ae40590f48b3315ecc3f86385e87
Reid <130939143+MichaelAnthonyReidJr@users.noreply.github.com>
2023-08-18 14:07:16 -0400

    Dev mr fix connection (#20)
    * Remove map from KPI, drop database code, and remove deprecation errors by
    
    * Fix connection to database. Populate database. Comment out code

--- server/index.js
+++ server/index.js
@@ -1,22 +1,35 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import helmet from "helmet";
-import morgan from "morgan"
-import kpiRoutes from "./routes/kpi.js";
+import express from "express"; //building apis
+import bodyParser from "body-parser"; //parse the body of incoming requests to JS
+import mongoose from "mongoose"; //defines models with a schema for MongoDB
+import cors from "cors"; //cross origin requests
+import dotenv from "dotenv"; //loads info from .env files
+import helmet from "helmet"; //secures app by setting headers
+import morgan from "morgan"; //logs requests to the terminal
+
+//import routes
+import kpiRoutes from "./routes/kpi.js"; 
 import productRoutes from "./routes/product.js";
 import transactionRoutes from "./routes/transaction.js";
+
+
+//MODELS IMPORTED FOR DATABASE DROP
+// import KPI from "./models/KPI.js"; 
+// import Product from "./models/Product.js";
+// import Transaction from "./models/Transaction.js";
+
 import KPI from "./models/KPI.js";
 import Product from "./models/Product.js";
 import Transaction from "./models/Transaction.js";
 import { kpis, products, transactions } from "./data/data.js";
 
-dotenv.config();
+//DATA IMPORTED FROM data.js FILE FOR INITIAL COMMIT
+// import { kpis, products, transactions } from "./data/data.js";
 
-const app = express();
+dotenv.config(); //loads environmenent
 
+const app = express(); //initializes instance of the express application to define routes, middleware, and start the serverr
+
+//configure express
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
@@ -25,6 +38,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+//mount the routes with the path and the function 
 app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes)
 app.use("/transaction", transactionRoutes);
@@ -39,12 +53,21 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(async () => {
+  
+    app.listen(PORT, () => console.log(`MongoDB is running......Server is Running on: http://localhost:${PORT}`));
+      //Drop the database for the initial commit to MONGODB    
+      // await mongoose.connection.db.dropDatabase();
+      // KPI.insertMany(kpis);
+      // Product.insertMany(products);
+      // Transaction.insertMany(transactions);
+  
     app.listen(PORT, () => console.log(`MongoDB is running......Server is Running on: ${PORT}`));
          
       await mongoose.connection.db.dropDatabase();
       KPI.insertMany(kpis);
       Product.insertMany(products);
       Transaction.insertMany(transactions);
+  
     })
   .catch((error) => {
     console.error("MongoDB connection error:", error.message);
