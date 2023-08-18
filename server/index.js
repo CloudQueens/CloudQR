f123997dc73229883fe20e583863a35107a79f55
Farjana Akter <fakter@xpanxion.com>
2023-08-17 20:41:05 -0400

    update my all file

--- server/index.js
+++ server/index.js
@@ -4,11 +4,14 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
-import morgan from "morgan"
+import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
 import productRoutes from "./routes/product.js";
-import Product from "./models/KPI.js";
-
+import transactionRoutes from "./routes/transaction.js";
+import KPI from "./models/KPI.js";
+import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 dotenv.config();
 
@@ -41,3 +44,7 @@ mongoose
   .catch((error) => {
     console.error("MongoDB connection error:", error.message);
   });
+
+
+
+
